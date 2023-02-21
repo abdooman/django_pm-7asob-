1bc499f0150fc3476871021fe44d1514e0fcf084
abdooman <maan1020@hotmail.com>
2023-02-21 11:08:57 +0300

    Search and pagination

--- projects/forms.py
+++ projects/forms.py
@@ -1,12 +1,26 @@
 from django import forms 
 from . import models
 
+attrs = {'class': 'form-control '}
+
 class ProjectCreateForm(forms.ModelForm):
     class Meta:
         model = models.Project
         fields = ['category', 'title', 'description']
         widgets = {
-            'category': forms.Select(),
-            'title': forms.TextInput(),
-            'dscription': forms.Textarea()
+            'category': forms.Select(attrs=attrs),
+            'title': forms.TextInput(attrs=attrs),
+            'dscription': forms.Textarea(attrs=attrs)
         }
+
+
+class ProjectUpdateForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['category', 'title', 'status']
+        widgets = {
+            'category': forms.Select(attrs=attrs),
+            'title': forms.TextInput(attrs=attrs),
+            'status': forms.Select(attrs=attrs)
+        }
+
--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,67 @@
-from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from . import models , forms
 
 class ProjectListView(ListView):
     model = models.Project
     template_name = 'project/list.html'
+    paginate_by = 4
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        where = {}
+        q = self.request.GET.get('q', None)
+        if q: 
+            where['title__icontains'] = q
+        return query_set.filter(**where)
+    
 
 class ProjectCreateView(CreateView):
     model = models.Project
     form_class = forms.ProjectCreateForm
     template_name = 'project/create.html'
     success_url = reverse_lazy('Project_list')
+
+
+class ProjectUpdateView(UpdateView):
+    model = models.Project
+    form_class = forms.ProjectUpdateForm
+    template_name = 'project/update.html'
+   
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.id])  # type: ignore
+    
+class ProjectDeleteView(DeleteView):
+    model = models.Project
+    template_name = 'project/delete.html'
+    success_url = reverse_lazy('Project_list')
+
+
+class TaskCreateView(CreateView):
+    model = models.Task
+    fields = ['project', 'description']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id]) # type: ignore
+        
+
+class TaskUpdateView(UpdateView):
+    model = models.Task
+    fields = ['is_completed']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+    
+
+
+class TaskDeleteView(DeleteView):
+    model = models.Task
+
+
+    def get_success_url(self):
+        return reverse('Project_update', args=[self.object.project.id])
+    
+
+
